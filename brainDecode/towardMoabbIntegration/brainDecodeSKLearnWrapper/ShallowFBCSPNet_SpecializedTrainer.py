8ea4b902d21c60735fd831a7f51a828193ec3d4e
atom2626 <atom2626@hotmail.com>
2018-06-15 21:21:39 -0400

    Exposed some parameters for the specialized trainer

--- brainDecode/towardMoabbIntegration/brainDecodeSKLearnWrapper/ShallowFBCSPNet_SpecializedTrainer.py
+++ brainDecode/towardMoabbIntegration/brainDecodeSKLearnWrapper/ShallowFBCSPNet_SpecializedTrainer.py
@@ -31,6 +31,17 @@ class ShallowFBCSPNet_SpecializedTrainer(BaseEstimator, ClassifierMixin):
         else:
             print("unsupported option")
             sys.exit(-1)
+            
+        # set default parameters
+        self.configure()    
+    
+    def configure(self, 
+                  nb_epoch=160, 
+                  initial_lr=0.00006
+                  trainTestRatio=7/8):
+        self.nb_epoch = nb_epoch
+        self.lr = initial_lr
+        self.trainTestRatio = trainTestRatio
 
     def _decorateNetwork(self, network):
         
@@ -56,7 +67,7 @@ class ShallowFBCSPNet_SpecializedTrainer(BaseEstimator, ClassifierMixin):
         n_filters_spat=5
         pool_time_length=60
         pool_time_stride=30
-        nb_epoch=160
+        
         
         # final_conv_length = auto ensures we only get a single output in the time dimension
         self.model = ShallowFBCSPNet(
@@ -89,29 +100,27 @@ class ShallowFBCSPNet_SpecializedTrainer(BaseEstimator, ClassifierMixin):
     """
     def fit(self, X, y):
         
-        nb_epoch=160
+        self.nb_epoch=160
         
         # prepare an optimizer
-        self.optimizer = optim.Adam(self.model.conv_classifier.parameters(),lr=0.00006)
-        
+        self.optimizer = optim.Adam(self.model.conv_classifier.parameters(),lr=self.lr)
         
         # define a number of train/test trials
-        nb_train_trials = int(np.floor(7/8*X.shape[0]))
+        nb_train_trials = int(np.floor(self.trainTestRatio*X.shape[0]))
         
         # split the dataset
         train_set = SignalAndTarget(X[:nb_train_trials], y=y[:nb_train_trials])
         test_set = SignalAndTarget(X[nb_train_trials:], y=y[nb_train_trials:])
         
-        
         # random generator
         self.rng = RandomState(None)
         
         # array that tracks results
-        self.loss_rec = np.zeros((nb_epoch,2))
-        self.accuracy_rec = np.zeros((nb_epoch,2))
+        self.loss_rec = np.zeros((self.nb_epoch,2))
+        self.accuracy_rec = np.zeros((self.nb_epoch,2))
                 
         # run all epoch
-        for i_epoch in range(nb_epoch):
+        for i_epoch in range(self.nb_epoch):
             
             self._batchTrain(i_epoch, train_set)
             self._evalTraining(i_epoch, train_set, test_set)
@@ -252,7 +261,7 @@ class ShallowFBCSPNet_SpecializedTrainer(BaseEstimator, ClassifierMixin):
 
     def adjust_learning_rate(self, optimizer, epoch):
         """Sets the learning rate to the initial LR decayed by 10% every 30 epochs"""
-        lr = 0.00006 * (0.1 ** (epoch // 30))
+        lr = self.lr * (0.1 ** (epoch // 30))
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr
 
@@ -261,111 +270,3 @@ class ShallowFBCSPNet_SpecializedTrainer(BaseEstimator, ClassifierMixin):
 
 
 
-
-
-
-"""
-
-
-print(model)
-
-# save/load only the model parameters(prefered solution)
-torch.save(model.state_dict(), "myModel.pth")
-
-# load the saved network (makes it possible to run bottom form same starting point 
-model.load_state_dict(torch.load("myModel.pth"))
-
-#from google.colab import files
-#files.download('example.txt') 
-
-
-
-from torch.nn import init
-
-    
-    
-
-from braindecode.torch_ext.util import np_to_var, var_to_np
-from braindecode.datautil.iterators import get_balanced_batches
-import torch.nn.functional as F
-from numpy.random import RandomState
-rng = RandomState(None)
-#rng = RandomState((2017,6,30))
-
-nb_epoch = 100
-loss_rec = np.zeros((nb_epoch,2))
-accuracy_rec = np.zeros((nb_epoch,2))
-
-
-
-
-for i_epoch in range(nb_epoch):
-    i_trials_in_batch = get_balanced_batches(len(train_set.X), rng, shuffle=True,
-                                            batch_size=32)
-    
-    
-    adjust_learning_rate(optimizer,i_epoch)
-    
-    
-    # Set model to training mode
-    model.train()
-    
-    
-    for i_trials in i_trials_in_batch:
-        # Have to add empty fourth dimension to X
-        batch_X = train_set.X[i_trials][:,:,:,None]
-        batch_y = train_set.y[i_trials]
-        net_in = np_to_var(batch_X)
-        if cuda:
-            net_in = net_in.cuda()
-        net_target = np_to_var(batch_y)
-        if cuda:
-            net_target = net_target.cuda()
-        # Remove gradients of last backward pass from all parameters
-        optimizer.zero_grad()
-        # Compute outputs of the network
-        outputs = model(net_in)
-        # Compute the loss
-        loss = F.nll_loss(outputs, net_target)
-        # Do the backpropagation
-        loss.backward()
-        # Update parameters with the optimizer
-        self.optimizer.step()
-
-    # Print some statistics each epoch
-    model.eval()
-    print("Epoch {:d}".format(i_epoch))
-    
-    sets = {'Train' : 0, 'Test' : 1}
-    for setname, dataset in (('Train', train_set), ('Test', test_set)):
-        i_trials_in_batch = get_balanced_batches(len(dataset.X), rng, batch_size=32, shuffle=False)
-        outputs = []
-        net_targets = []
-        for i_trials in i_trials_in_batch:
-            batch_X = dataset.X[i_trials][:,:,:,None]
-            batch_y = dataset.y[i_trials]
-            
-            net_in = np_to_var(batch_X)
-            if cuda:
-                net_in = net_in.cuda()
-            net_target = np_to_var(batch_y)
-            if cuda:
-                net_target = net_target.cuda()
-            net_target = var_to_np(net_target)
-            output = var_to_np(model(net_in))
-            outputs.append(output)
-            net_targets.append(net_target)
-        net_targets = np_to_var(np.concatenate(net_targets))
-        outputs = np_to_var(np.concatenate(outputs))
-        loss = F.nll_loss(outputs, net_targets)
- 
-        print("{:6s} Loss: {:.5f}".format(
-            setname, float(var_to_np(loss))))
-        loss_rec[i_epoch, sets[setname]] = var_to_np(loss)
-    
-        predicted_labels = np.argmax(var_to_np(outputs), axis=1)
-        accuracy = np.mean(dataset.y  == predicted_labels)
-        print("{:6s} Accuracy: {:.1f}%".format(
-            setname, accuracy * 100))
-        accuracy_rec[i_epoch, sets[setname]] = accuracy
-"""
